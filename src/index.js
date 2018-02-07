8ad4ecfb613e7849d92f417d3439a30cd37f89d6
Andrei Astakhov <A.Astakhov@bssys.com>
2018-02-08 00:26:28 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+﻿const http = require('http');
+const questions = require('./questions');
+
+const server = http.createServer((request, response) => {
+    const requestParameters = request.url.split('/');
+    let responseData = null;
+
+    switch (requestParameters[1]) {
+        case 'questions':
+            switch (requestParameters[2]) {
+                // questions/count
+                case 'count':
+                    responseData = questions.length;
+                    break;
+                // questions/id/{id}
+                case 'id':
+                    responseData = requestParameters[3] && questions[requestParameters[3]];
+                    break;
+                default:
+                    break;
+            }
+        default:
+            break;
+    }
+
+    response.setHeader('Content-Type', 'application/json');
+    response.end(JSON.stringify(responseData));
+});
+
+server.listen(3000);
---
+++ src/questions.js
@@ -0,0 +1,12 @@
+﻿const questions = [
+  {
+      answer: 'тестовый ответ 1',
+      question: 'тестовый вопрос 1',
+  },
+  {
+      answer: 'тестовый ответ 2',
+      question: 'тестовый вопрос 2',
+  }
+];
+
+module.exports = questions;
